6455a0d5ccb73f1d488d69143902368787b0e1b9
poliHorser <134537683+poliHorser@users.noreply.github.com>
2023-10-14 20:57:42 +0300

    j

---
+++ src/js/01-gallery.js
@@ -0,0 +1,38 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+Prevent.default(
+  console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+const item = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link"  href="${original}">
+        <img
+        loading="lazy"
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+}).join("");
+gallery.insertAdjacentHTML("beforeend", item)
+gallery.addEventListener("click", imageClick)
+
+  
+  
+  function onImageClick(event) {
+     event.preventDefault();
+     const target = event.target;
+     if (target.classList.contains('gallery__image')) {
+         const originalImageUrl = target.dataset.source;
+         modal = basicLightbox.create(`
+         <img src="${originalImageUrl}" width="800" height="600">
+         `);
+         modal.show();
+         modalIsOpen = true;
+     }
+     document.addEventListener('keydown', keyDown);
+}
+ )
