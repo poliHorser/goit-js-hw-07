0676d2d65c7fc22457734c11e6dac430c7be7fe1
poliHorser <134537683+poliHorser@users.noreply.github.com>
2023-10-15 16:10:49 +0300

    fin

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(galleryItems);
+
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>
+//             Your first lightbox with just a few lines of code.
+//             Yes, it's really that simple.
+//         </p>
+//     </div>
+// `)
+
+// instance.show()
+
 const gallery = document.querySelector(".gallery")
 const item = galleryItems.map(({ preview, original, description }) => {
     return `<li class="gallery__item">
   <a class="gallery__link"  href="${original}">
         <img
-        loading="lazy"
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
@@ -15,18 +27,20 @@ const item = galleryItems.map(({ preview, original, description }) => {
   </a>
 </li>`;
 }).join("");
-gallery.insertAdjacentHTML("beforeend", item)
+gallery.insertAdjacentHTML("afterbegin", item)
 gallery.addEventListener("click", imageClick)
-function onImageClick(event) {
+
+function imageClick(event) {
      event.preventDefault();
      const target = event.target;
      if (target.classList.contains('gallery__image')) {
          const originalImageUrl = target.dataset.source;
-         modal = basicLightbox.create(`
-         <img src="${originalImageUrl}" width="800" height="600">
+         const modal = basicLightbox.create(`
+         <img 
+         src="${originalImageUrl}" 
+         >
          `);
-         modal.show();
-         modalIsOpen = true;
+      modal.show();
+      
      }
-     document.addEventListener('keydown', keyDown);
- }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector(".gallery")
+const markUp = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+}).join("")
+
+listEl.insertAdjacentHTML("beforeend", markUp)
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true, // Поправлено опцію на "captions"
+  captionDelay: 250,
+  fadeSpeed: 250,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
+
